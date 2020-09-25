913eeefeec491b9568cc47802b9b79ab2a145e71
Bekarysalashybayev <bekarysalashybaev@gmail.com>
2020-09-25 16:36:59 +0600

    Updates

---
+++ precache-manifest.fd81a201a75032360b86e145654c3afa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "991fa4f3710c6ba7f00fd8226fc874f4",
+    "url": "/WeatherSimpleReactApp/index.html"
+  },
+  {
+    "revision": "662b78d99da2434f138d",
+    "url": "/WeatherSimpleReactApp/static/css/main.8515258c.chunk.css"
+  },
+  {
+    "revision": "8a6bf7ccecbbff4619e0",
+    "url": "/WeatherSimpleReactApp/static/js/2.10d4690c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherSimpleReactApp/static/js/2.10d4690c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "662b78d99da2434f138d",
+    "url": "/WeatherSimpleReactApp/static/js/main.7157d350.chunk.js"
+  },
+  {
+    "revision": "495469fd45d0ac683e95",
+    "url": "/WeatherSimpleReactApp/static/js/runtime-main.d547cc50.js"
+  }
+]);
